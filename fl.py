0e2649753695ad289b1291545474b48c3f129872
kislotniq <kislotniq@gmail.com>
2018-10-02 21:33:40 +0200

    Basic flask routing
      - listing fake customers
      - getting a fake customer name by id

--- fl.py
+++ fl.py
@@ -1,7 +1,27 @@
 from flask import Flask
+import string
 
 app = Flask(__name__)
 
-@app.route('/')
-def helloWorld():
-    return 'hello world!\n'
+
+
+def listCustomers():
+    return [42, 93, 84, 13]
+
+def getCustomerName(identifier):
+    return 'John'
+
+
+
+@app.route('/customers')
+def presentationListCustomers():
+    customer_ids = listCustomers()
+    stringified_ids = [str(x) for x in customer_ids]
+    return string.join(stringified_ids, '\n')
+
+
+@app.route('/customers/<int:id>')
+def presentationGetCustomer(id):
+    # No need to sanitize id, looks like flask
+    # properly handles non-integer values
+    return getCustomerName(id) + '\n'
