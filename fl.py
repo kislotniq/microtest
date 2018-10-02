be4d826c9bf0239caa26a92fae0497cc27e7a77d
kislotniq <kislotniq@gmail.com>
2018-10-02 22:02:50 +0200

    PUT method to set customer name
      fully workable PUT to set customer name, but the database
      functionality is mock.

--- fl.py
+++ fl.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import request
+from flask import abort
+
 import string
 
 app = Flask(__name__)
@@ -10,6 +13,20 @@ def listCustomers():
 
 def getCustomerName(identifier):
     return 'John'
+
+def setNewCustomerName(identifier, new_name):
+    pass
+
+
+def nameIsAllowed(name):
+    """
+    Returns true if name consists of lower case
+    latin letters
+    """
+    for character in name:
+        if ord(character) < ord('a') or ord(character) > ord('z'):
+            return False
+    return True
 
 
 
@@ -25,3 +42,16 @@ def presentationGetCustomer(id):
     # No need to sanitize id, looks like flask
     # properly handles non-integer values
     return getCustomerName(id) + '\n'
+
+
+@app.route('/customers/<int:id>', methods=['PUT'])
+def presentationSetCustomerName(id):
+    new_name = request.args.get("newName")
+
+    if not new_name:
+        abort(400, 'Error: missing the newName argument')
+    if not nameIsAllowed(new_name):
+        abort(400, 'Error: only lower case latin letters for newName allowed')
+
+    setNewCustomerName(id, new_name)
+    return "Ok, new name: {n}\n".format(n=new_name)
